88408c80de6ec10a9bd495f174070c4ecf4e5099
Neikox <k.suarezsanchez1991@gmail.com>
2020-10-16 16:32:08 +0100

    Updates

---
+++ javascript.f88acecf.js
@@ -0,0 +1,30 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"BHn7":[function(require,module,exports) {
+module.exports="/octave/ogh1.e7d9caee.mp3";
+},{}],"rlYW":[function(require,module,exports) {
+module.exports="/octave/ogh10.2f70dae2.mp3";
+},{}],"wVfA":[function(require,module,exports) {
+module.exports="/octave/ogh11.65bcab5e.mp3";
+},{}],"tubG":[function(require,module,exports) {
+module.exports="/octave/ogh12.898b8a69.mp3";
+},{}],"HdQY":[function(require,module,exports) {
+module.exports="/octave/ogh2.54fd76e8.mp3";
+},{}],"nacW":[function(require,module,exports) {
+module.exports="/octave/ogh3.eedf7472.mp3";
+},{}],"jb78":[function(require,module,exports) {
+module.exports="/octave/ogh4.a563951b.mp3";
+},{}],"CJ4y":[function(require,module,exports) {
+module.exports="/octave/ogh5.43b28b87.mp3";
+},{}],"hIK5":[function(require,module,exports) {
+module.exports="/octave/ogh6.8c32b3fe.mp3";
+},{}],"eXMa":[function(require,module,exports) {
+module.exports="/octave/ogh7.75a1daf0.mp3";
+},{}],"VlKc":[function(require,module,exports) {
+module.exports="/octave/ogh8.d1b16d08.mp3";
+},{}],"MNZg":[function(require,module,exports) {
+module.exports="/octave/ogh9.7dbc6a6d.mp3";
+},{}],"agu0":[function(require,module,exports) {
+module.exports={ogh1:require("./ogh1.mp3"),ogh10:require("./ogh10.mp3"),ogh11:require("./ogh11.mp3"),ogh12:require("./ogh12.mp3"),ogh2:require("./ogh2.mp3"),ogh3:require("./ogh3.mp3"),ogh4:require("./ogh4.mp3"),ogh5:require("./ogh5.mp3"),ogh6:require("./ogh6.mp3"),ogh7:require("./ogh7.mp3"),ogh8:require("./ogh8.mp3"),ogh9:require("./ogh9.mp3")};
+},{"./ogh1.mp3":"BHn7","./ogh10.mp3":"rlYW","./ogh11.mp3":"wVfA","./ogh12.mp3":"tubG","./ogh2.mp3":"HdQY","./ogh3.mp3":"nacW","./ogh4.mp3":"jb78","./ogh5.mp3":"CJ4y","./ogh6.mp3":"hIK5","./ogh7.mp3":"eXMa","./ogh8.mp3":"VlKc","./ogh9.mp3":"MNZg"}],"ZZvM":[function(require,module,exports) {
+"use strict";var e=t(require("../assets/*.mp3"));function t(e){return e&&e.__esModule?e:{default:e}}var n=document.querySelector(".g");n.addEventListener("click",function(){new Audio(e.default.ogh1).play()});var u=document.querySelector(".fs");u.addEventListener("click",function(){new Audio(e.default.ogh2).play()});var c=document.querySelector(".f");c.addEventListener("click",function(){new Audio(e.default.ogh3).play()});var d=document.querySelector(".e");d.addEventListener("click",function(){new Audio(e.default.ogh4).play()});var o=document.querySelector(".ds");o.addEventListener("click",function(){new Audio(e.default.ogh5).play()});var r=document.querySelector(".d");r.addEventListener("click",function(){new Audio(e.default.ogh6).play()});var a=document.querySelector(".cs");a.addEventListener("click",function(){new Audio(e.default.ogh7).play()});var i=document.querySelector(".c");i.addEventListener("click",function(){new Audio(e.default.ogh8).play()});var l=document.querySelector(".b");l.addEventListener("click",function(){new Audio(e.default.ogh9).play()});var f=document.querySelector(".as");f.addEventListener("click",function(){new Audio(e.default.ogh10).play()});var v=document.querySelector(".a");v.addEventListener("click",function(){new Audio(e.default.ogh11).play()});var y=document.querySelector(".gs");y.addEventListener("click",function(){new Audio(e.default.ogh12).play()});
+},{"../assets/*.mp3":"agu0"}]},{},["ZZvM"], null)
+//# sourceMappingURL=/octave/javascript.f88acecf.js.map
