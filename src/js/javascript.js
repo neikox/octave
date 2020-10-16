6204be6006fb06f4b41f40ff2df06e82855cb999
Neikox <k.suarezsanchez1991@gmail.com>
2020-10-16 15:50:33 +0100

    first commit

---
+++ src/js/javascript.js
@@ -0,0 +1,62 @@
+import audios from "../assets/*.mp3";
+
+const ogh1 = document.querySelector(".g");
+ogh1.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh1);
+    bew.play();
+});
+const ogh2 = document.querySelector(".fs");
+ogh2.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh2);
+    bew.play();
+});
+const ogh3 = document.querySelector(".f");
+ogh3.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh3);
+    bew.play();
+});
+const ogh4 = document.querySelector(".e");
+ogh4.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh4);
+    bew.play();
+});
+const ogh5 = document.querySelector(".ds");
+ogh5.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh5);
+    bew.play();
+});
+const ogh6 = document.querySelector(".d");
+ogh6.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh6);
+    bew.play();
+});
+const ogh7 = document.querySelector(".cs");
+ogh7.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh7);
+    bew.play();
+});
+const ogh8 = document.querySelector(".c");
+ogh8.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh8);
+    bew.play();
+});
+const ogh9 = document.querySelector(".b");
+ogh9.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh9);
+    bew.play();
+});
+const ogh10 = document.querySelector(".as");
+ogh10.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh10);
+    bew.play();
+});
+const ogh11 = document.querySelector(".a");
+ogh11.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh11);
+    bew.play();
+});
+const ogh12 = document.querySelector(".gs");
+ogh12.addEventListener("click", () => {
+    const bew = new Audio(audios.ogh12);
+    bew.play();
+});
